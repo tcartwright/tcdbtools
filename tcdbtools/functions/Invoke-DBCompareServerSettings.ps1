758295d5328f89f8f457ddf181b9c4f56b070201
Tim Cartwright <tcartwright@users.noreply.github.com>
2022-12-12 09:37:17 -0500

    - added breakdown of options to server compare

--- tcdbtools/functions/Invoke-DBCompareServerSettings.ps1
+++ tcdbtools/functions/Invoke-DBCompareServerSettings.ps1
@@ -44,7 +44,53 @@
         $SqlCmdArguments = $sqlCon.SqlCmdArguments
 
         $compareServers =  $ServerInstances | ForEach-Object { ($_).ToUpper() }
-        $query = "SELECT Name, Value FROM sys.configurations ORDER BY name"
+        $query = "
+    DECLARE @options TABLE ([name] nvarchar(35), [minimum] int, [maximum] int, [config_value] int, [run_value] int)
+    DECLARE @optionsCheck TABLE([id] int NOT NULL IDENTITY, [setting_name] varchar(128))
+    DECLARE @current_value INT;
+    
+    INSERT INTO @options ([name], [minimum], [maximum], [config_value], [run_value])
+    EXEC sp_configure 'user_options';
+    
+    SELECT @current_value = [config_value] FROM @options;
+    
+    INSERT INTO @optionsCheck 
+        ([setting_name]) 
+    VALUES
+        ('DISABLE_DEF_CNST_CHK'),
+        ('IMPLICIT_TRANSACTIONS'),
+        ('CURSOR_CLOSE_ON_COMMIT'),
+        ('ANSI_WARNINGS'),
+        ('ANSI_PADDING'),
+        ('ANSI_NULLS'),
+        ('ARITHABORT'),
+        ('ARITHIGNORE'),
+        ('QUOTED_IDENTIFIER'),
+        ('NOCOUNT'),
+        ('ANSI_NULL_DFLT_ON'),
+        ('ANSI_NULL_DFLT_OFF'),
+        ('CONCAT_NULL_YIELDS_NULL'),
+        ('NUMERIC_ROUNDABORT'),
+        ('XACT_ABORT')
+    
+    ;WITH settings AS (
+        SELECT 
+            [name], [value] 
+        FROM sys.configurations c
+        ORDER BY [name]
+        OFFSET 0 ROWS
+    )
+    
+    SELECT * 
+    FROM [settings]
+        UNION ALL
+    SELECT CONCAT(oc.[setting_name], ' (options)'),   
+        [server_option] = CASE WHEN (@current_value & fn.[value]) = fn.[value] THEN 1 ELSE 0 END
+    FROM @optionsCheck oc
+    CROSS APPLY (
+        SELECT [value] = CASE WHEN oc.id > 1 THEN POWER(2, oc.id - 1) ELSE 1 END
+    ) fn        
+        "
     }
 
     process {
